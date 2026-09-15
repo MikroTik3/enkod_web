4525dbeb0c4b1ed26314b27e6027d6cb4951f85e
Artur Docenko <dotsenk20034@gmail.com>
2026-09-15 23:20:14 +0300

    chore: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,14 +4,14 @@ import { getCourses } from '../api/requests'
 import { APP_CONFIG } from '../constants'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-	const courses: MetadataRoute.Sitemap = (await getCourses()).map(
-		course => ({
-			url: `${APP_CONFIG.baseUrl}/${course.slug}`,
-			lastModified: new Date(),
-			changeFrequency: 'monthly',
-			priority: 0.9
-		})
-	)
+	const coursesData = await getCourses().catch(() => [])
+
+	const courses: MetadataRoute.Sitemap = coursesData.map(course => ({
+		url: `${APP_CONFIG.baseUrl}/${course.slug}`,
+		lastModified: new Date(),
+		changeFrequency: 'monthly',
+		priority: 0.9
+	}))
 
 	return [
 		{
